2a404668ccd2f69e9bef13ed6ad1975a73e465a1
Sauls <sauls.me@gmail.com>
2013-12-16 22:31:11 +0800

    Chore: Update app.js

--- app.js
+++ app.js
@@ -15,17 +15,71 @@ app.configure(function() {
 });
 
 
+mongoose.connect("mongodb://localhost/todo");
+
+var Schema = mongoose.Schema;
+
+var todoSchema = new Schema({
+    title: String,
+    completed: Boolean
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
 // get
+app.get("/todo", function(req, res) {
+    Todo.find({}, function(err, docs) {
+        res.send(docs);
+    });
+});
 
+// create
+app.post('/todo', function(req, res) {
+    var b = req.body;
+    new Todo({
+        title: b.title,
+        completed: b.completed
+    }).save(function(err, docs){
+        if (err)
+            res.json(err);
+    });
+});
 
-// add
+// handle param
+app.param('item', function(req, res, next, name) {
+    Todo.find({ title: item }, function(err, docs) {
+        req.todo = doc[0];
+        next();
+    });
+});
 
+app.get('/todo/:item', function(req, res) {
+    res.send(req.todo);
+});
 
 // update
+app.put('/todo/:item', function(req, res) {
+    var b = req.body;
+    Todo.update(
+        // query
+        {title: req.param.title},
+        {title: b.title, completed: b.completed},
+        function(err) {
 
+        }
+    );
+})
 
 // delete
+app.delete('/todo/:item', function(req, res) {
+    Todo.remove(
+        // query
+        {title: req.params.title},
+        function(err) {
 
+        }
+    )
+});
 
 
 
